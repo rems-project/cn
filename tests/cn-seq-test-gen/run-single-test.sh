769cb7db20c70e579fa2e8259c99f84f87cda78c
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-05-17 01:28:13 -0400

    Parallelize Seq-Test CI

---
+++ tests/cn-seq-test-gen/run-single-test.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+CN=$OPAM_SWITCH_PREFIX/bin/cn
+
+DIRNAME=$(dirname "$0")
+TEST=$1
+
+# Clean directory
+cd "$DIRNAME" || exit
+if [[ $TEST == *.pass.c ]]; then
+  DIR=passing-$(basename $TEST .pass.c)
+else
+  DIR=failing-$(basename $TEST .fail.c)
+fi
+rm -rf $DIR
+
+# For stricter CI
+export CPPFLAGS="${CPPFLAGS} -Werror"
+
+# For UBSan
+export UBSAN_OPTIONS=halt_on_error=1
+
+# Track failures
+NUM_FAILED=0
+FAILED=''
+
+function separator() {
+  OUTPUT="${OUTPUT}"$'\n===========================================================\n\n'
+}
+
+CONFIGS=("--num-samples=30 --max-resets=1")
+
+OUTPUT=""
+
+# For each configuration
+for CONFIG in "${CONFIGS[@]}"; do
+  separator
+  OUTPUT="${OUTPUT}Running CI with CLI config \"$CONFIG\""$'\n'
+  separator
+
+  if [[ $TEST == *.pass.c ]]; then
+    CLEANUP="rm -rf ${DIR} run_tests.sh;separator"
+    OUTPUT="${OUTPUT}$($CN seq-test "$TEST" --output-dir="$DIR" $CONFIG 2>&1)"
+    RET=$?
+    if [[ "$RET" != 0 ]]; then
+      OUTPUT="${OUTPUT}"$'\n'"$TEST -- Tests failed unexpectedly"$'\n'
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED ($ALT_CONFIG)"
+    else
+      OUTPUT="${OUTPUT}"$'\n'"$TEST -- Tests passed successfully"$'\n'
+    fi
+  elif [[ $TEST == *.fail.c ]]; then
+    CLEANUP="rm -rf ${DIR} run_tests.sh;separator"
+    THIS_OUTPUT=$($CN seq-test "$TEST" --output-dir="$DIR" $CONFIG 2>&1)
+    RET=$?
+    if [[ "$RET" == 0 ]]; then
+      OUTPUT="${OUTPUT}\n$TEST -- Tests passed unexpectedly\n"
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED ($ALT_CONFIG)"
+    elif [[ "$RET" != 123 ]]; then
+      OUTPUT="${OUTPUT}${THIS_OUTPUT}\n$TEST -- Tests failed unnaturally\n"
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED ($ALT_CONFIG)"
+    else
+      OUTPUT="${OUTPUT}"$'\n'"$TEST -- Tests failed successfully"$'\n'
+    fi
+  elif [[ $TEST == *.flaky.c ]]; then
+    CLEANUP="rm -rf ${DIR} run_tests.sh;separator"
+    THIS_OUTPUT=$($CN seq-test "$TEST" --output-dir="$DIR" $CONFIG 2>&1)
+    RET=$?
+
+    # Run twice, since flaky
+    if [[ "$RET" == 0 ]]; then
+      THIS_OUTPUT=$($CN seq-test "$TEST" --output-dir="$DIR" $CONFIG 2>&1)
+      RET=$?
+    fi
+
+    if [[ "$RET" == 0 ]]; then
+      OUTPUT="${OUTPUT}\n$TEST -- Tests passed unexpectedly\n"
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED ($ALT_CONFIG)"
+    elif [[ "$RET" != 1 ]]; then
+      OUTPUT="${OUTPUT}${THIS_OUTPUT}\n$TEST -- Tests failed unnaturally\n"
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED ($ALT_CONFIG)"
+    else
+      OUTPUT="${OUTPUT}\n$TEST -- Tests failed successfully"
+    fi
+  fi
+
+  eval "$CLEANUP"
+done
+
+if [ -z "$FAILED" ]; then
+  echo "$TEST - all configs passed."
+  exit 0
+else
+  OUTPUT="${OUTPUT}$TEST - $NUM_FAILED configs failed:\n  $FAILED"
+  printf "%b\n" "${OUTPUT}"
+  exit 1
+fi
