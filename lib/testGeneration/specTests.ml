83ae9881b728a97a2d944c9e5a8e9b5c31e91346
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-05-30 03:05:47 -0400

    Move generator synthesis into a submodule

--- lib/testGeneration/specTests.ml
+++ lib/testGeneration/specTests.ml
@@ -8,30 +8,6 @@ module Utils = Fulminate.Utils
 module FExtract = Fulminate.Extract
 module Config = TestGenConfig
 
-let debug_log_file : out_channel option ref = ref None
-
-let init_debug () =
-  if Option.is_none !debug_log_file && !Cerb_debug.debug_level > 0 then
-    debug_log_file
-    := Some
-         (let open Stdlib in
-          open_out "generatorCompilation.log")
-
-
-let debug_log (str : string) : unit =
-  init_debug ();
-  match !debug_log_file with
-  | Some oc ->
-    output_string oc str;
-    flush oc
-  | None -> ()
-
-
-let debug_stage (stage : string) (str : string) : unit =
-  debug_log (stage ^ ":\n");
-  debug_log (str ^ "\n\n")
-
-
 let compile_constant_tests
       filename
       (sigma : CF.GenTypes.genTypeCategory A.sigma)
@@ -76,26 +52,6 @@ let compile_constant_tests
   in
   let open Pp in
   (tests, separate (twice hardline) docs ^^ twice hardline)
-
-
-let compile_generators
-      filename
-      (sigma : CF.GenTypes.genTypeCategory A.sigma)
-      (prog5 : unit Mucore.file)
-      (tests : Test.t list)
-  : Pp.document
-  =
-  let ctx = GenCompile.compile filename prog5.resource_predicates tests in
-  debug_stage "Compile" (ctx |> GenDefinitions.pp_context |> Pp.plain ~width:80);
-  let ctx = ctx |> GenNormalize.normalize prog5 in
-  debug_stage "Normalize" (ctx |> GenDefinitions.pp_context |> Pp.plain ~width:80);
-  let ctx = ctx |> GenDistribute.distribute in
-  debug_stage "Distribute" (ctx |> GenDefinitions.pp_context |> Pp.plain ~width:80);
-  let ctx = ctx |> GenOptimize.optimize prog5 in
-  debug_stage "Optimize" (ctx |> GenDefinitions.pp_context |> Pp.plain ~width:80);
-  let ctx = ctx |> GenElaboration.elaborate in
-  debug_stage "Elaborated" (ctx |> GenElaboration.pp |> Pp.plain ~width:80);
-  ctx |> GenCodeGen.compile sigma
 
 
 let convert_from ((x, ct) : Sym.t * C.ctype) =
