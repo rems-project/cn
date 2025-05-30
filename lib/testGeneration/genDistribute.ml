f3786d7e5d10be0c4f41b1b39636d0a072859540
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-05-30 02:21:45 -0400

    Disable max array length in generators

--- lib/testGeneration/genDistribute.ml
+++ lib/testGeneration/genDistribute.ml
@@ -1,9 +1,7 @@
 module BT = BaseTypes
 module IT = IndexTerms
-module LC = LogicalConstraints
 module GT = GenTerms
 module GD = GenDefinitions
-module Config = TestGenConfig
 
 let generated_size (bt : BT.t) : int =
   match bt with
@@ -25,42 +23,6 @@ let allocations (gt : GT.t) : GT.t =
     GT (gt_, bt, loc)
   in
   GT.map_gen_pre aux gt
-
-
-let apply_array_max_length (gt : GT.t) : GT.t =
-  let rec aux (gt : GT.t) : GT.t =
-    let (GT (gt_, _bt, here)) = gt in
-    match gt_ with
-    | Arbitrary | Uniform _ | Alloc _ | Call _ | Return _ -> gt
-    | Pick wgts -> GT.pick_ (List.map_snd aux wgts) here
-    | Asgn ((it_addr, sct), it_val, gt') ->
-      GT.asgn_ ((it_addr, sct), it_val, aux gt') here
-    | Let (backtracks, (x, gt_inner), gt') ->
-      GT.let_ (backtracks, (x, aux gt_inner), aux gt') here
-    | Assert (lc, gt') -> GT.assert_ (lc, aux gt') here
-    | ITE (it_if, gt_then, gt_else) -> GT.ite_ (it_if, aux gt_then, aux gt_else) here
-    | Map ((i, i_bt, it_perm), gt') ->
-      let _it_min, it_max = IndexTerms.Bounds.get_bounds (i, i_bt) it_perm in
-      let loc = Locations.other __LOC__ in
-      let it_max_min =
-        IT.le_
-          ( IT.num_lit_ (Z.of_int 0) (IT.get_bt it_max) loc,
-            IT.add_ (it_max, IT.num_lit_ Z.one (IT.get_bt it_max) loc) loc )
-          loc
-      in
-      let it_max_max =
-        IT.lt_
-          ( it_max,
-            IT.num_lit_ (Z.of_int (Config.get_max_array_length ())) (IT.get_bt it_max) loc
-          )
-          loc
-      in
-      GT.assert_
-        ( LC.T (IT.and2_ (it_max_min, it_max_max) loc),
-          GT.map_ ((i, i_bt, it_perm), aux gt') here )
-        loc
-  in
-  aux gt
 
 
 let default_weights (gt : GT.t) : GT.t =
@@ -102,7 +64,7 @@ let confirm_distribution (gt : GT.t) : GT.t =
 
 
 let distribute_gen (gt : GT.t) : GT.t =
-  gt |> allocations |> apply_array_max_length |> default_weights |> confirm_distribution
+  gt |> allocations |> default_weights |> confirm_distribution
 
 
 let distribute_gen_def ({ filename; recursive; spec; name; iargs; oargs; body } : GD.t)
