f91901cd1a9331647bfc6ac997f89238ab63cb36
Dimitrios Economou <dimitrios.j.economou@gmail.com>
2025-06-02 21:16:41 +0100

    Fix bad ghost ordering test

--- tests/cn/ghost_bad_ordering.error.c
+++ tests/cn/ghost_bad_ordering.error.c
@@ -1,7 +1,7 @@
 int foo(int x)
-/*@ requires x + n - n < MAXi32(); @*/
-/*@ requires ghost i32 n; true; @*/
-/*@ ensures return == x + 1i32; @*/
+/*@ requires x + n - n < MAXi32();
+    requires ghost i32 n; true;
+    ensures return == x + 1i32; @*/
 {
     return x + 1;
 }
