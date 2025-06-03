bc0473d52bfada429627bb97bb123fa08d221e11
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-06-02 23:23:49 -0400

    Add flipping ifs to experimental gens

--- lib/testGeneration/bennet/stage2/stage2.ml
+++ lib/testGeneration/bennet/stage2/stage2.ml
@@ -6,6 +6,7 @@ let transform (ctx : Stage1.Ctx.t) : Ctx.t =
   ctx
   |> Convert.transform
   |> LiftConstraints.transform
+  |> FlipIfs.transform
   |> Reorder.transform
   |> SpecializeEquality.transform
   |> SimplifyNames.transform
