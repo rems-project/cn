55d8d4f7bd490765515fc2fc44741c2de58af606
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-07-07 20:57:06 -0400

    Add more logging to generator opt passes

--- lib/testGeneration/bennet/stage2/specializeEquality.ml
+++ lib/testGeneration/bennet/stage2/specializeEquality.ml
@@ -66,4 +66,6 @@ let transform_gd (gd : Def.t) : Def.t =
   { gd with body = transform_gt vars gd.body }
 
 
-let transform (ctx : Ctx.t) : Ctx.t = List.map_snd transform_gd ctx
+let transform (ctx : Ctx.t) : Ctx.t =
+  Cerb_debug.print_debug 2 [] (fun () -> "specialize_equality");
+  List.map_snd transform_gd ctx
