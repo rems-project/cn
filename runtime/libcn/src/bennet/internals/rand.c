d7a02c086d019d4e06901b277a872154b0abe645
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-07-27 23:02:26 -0400

    Replace Mersenne Twister with SplitMix64 (#302)

--- runtime/libcn/src/bennet/internals/rand.c
+++ runtime/libcn/src/bennet/internals/rand.c
@@ -9,94 +9,25 @@
 #include <bennet/internals/rand.h>
 #include <bennet/internals/size.h>
 
-// Mersenne twister from https://doi.org/10.1145/369534.369540
+// SplitMix64 from https://doi.org/10.1145/2714064.2660195
+// Written in 2015 by Sebastiano Vigna (vigna@acm.org)
 
-/* Period parameters */
-#define NN 312
-#define M0 63
-#define M1 151
-#define M2 224
-/* Constant vector a */
-#define MATRIX_A 0xB3815B624FC82E2FULL
-/* Most significant 33 bits */
-#define UMASK 0xFFFFFFFF80000000ULL
-/* Least significant 31 bits */
-#define LMASK 0x7FFFFFFFULL
-
-/* Tempering parameters */
-#define MASK_B 0x599CFCBFCA660000ULL
-#define MASK_C 0xFFFAAFFE00000000ULL
-#define UU     26
-#define SS     17
-#define TT     33
-#define LL     39
-
-/* The array for the state vector */
-static unsigned long long mt[NN];
-/* mti55NN11 means mt[NN] is not initialized */
-static int mti = NN + 1;
+static uint64_t rng_state;
 
 void sgenrand(uint64_t seed) {
-  uint64_t ux, lx;
-  for (mti = 0; mti < NN; mti++) {
-    ux = seed & 0xFFFFFFFF00000000ULL;
-    seed = 2862933555777941757ULL * seed + 1ULL;
-    lx = seed >> 32;
-    seed = 2862933555777941757ULL * seed + 1ULL;
-    mt[mti] = ux | lx;
-  }
+  rng_state = seed;
 }
 
 uint64_t genrand(void) {
-  int32_t i;
-  uint64_t x;
-  static unsigned long long mag01[2] = {0ULL, MATRIX_A};
-
-  if (mti >= NN) { /* generate NN words at one time */
-    /* if sgenrand() has not been called, */
-    /* a default initial seed is used */
-    if (mti == NN + 1)
-      sgenrand(0xDEADCAFE);
-
-    for (i = 0; i < NN - M2; i++) {
-      x = (mt[i] & UMASK) | (mt[i + 1] & LMASK);
-      mt[i] = (x >> 1) ^ mag01[(int)(x & 1ULL)];
-      mt[i] ^= mt[i + M0] ^ mt[i + M1] ^ mt[i + M2];
-    }
-    for (; i < NN - M1; i++) {
-      x = (mt[i] & UMASK) | (mt[i + 1] & LMASK);
-      mt[i] = (x >> 1) ^ mag01[(int)(x & 1ULL)];
-      mt[i] ^= mt[i + M0] ^ mt[i + M1] ^ mt[i + M2 - NN];
-    }
-    for (; i < NN - M0; i++) {
-      x = (mt[i] & UMASK) | (mt[i + 1] & LMASK);
-      mt[i] = (x >> 1) ^ mag01[(int)(x & 1ULL)];
-      mt[i] ^= mt[i + M0] ^ mt[i + M1 - NN] ^ mt[i + M2 - NN];
-    }
-    for (; i < NN - 1; i++) {
-      x = (mt[i] & UMASK) | (mt[i + 1] & LMASK);
-      mt[i] = (x >> 1) ^ mag01[(int)(x & 1ULL)];
-      mt[i] ^= mt[i + M0 - NN] ^ mt[i + M1 - NN] ^ mt[i + M2 - NN];
-    }
-    x = (mt[NN - 1] & UMASK) | (mt[0] & LMASK);
-    mt[NN - 1] = (x >> 1) ^ mag01[(int)(x & 1ULL)];
-    mt[NN - 1] ^= mt[M0 - 1] ^ mt[M1 - 1] ^ mt[M2 - 1];
-
-    mti = 0;
-  }
-
-  x = mt[mti++];
-  x ^= (x >> UU);
-  x ^= (x << SS) & MASK_B;
-  x ^= (x << TT) & MASK_C;
-  x ^= (x >> LL);
-
-  return x;
+  uint64_t z = (rng_state += UINT64_C(0x9E3779B97F4A7C15));
+  z = (z ^ (z >> 30)) * UINT64_C(0xBF58476D1CE4E5B9);
+  z = (z ^ (z >> 27)) * UINT64_C(0x94D049BB133111EB);
+  return z ^ (z >> 31);
 }
 
-/////////////////////////////
-// End of Mersenne twister //
-/////////////////////////////
+///////////////////////
+// End of SplitMix64 //
+///////////////////////
 
 // Sized generation according to Lemire: https://doi.org/10.1145/3230636
 #define UNSIGNED_GEN(sm, lg)                                                             \
