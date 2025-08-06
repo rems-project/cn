b125a94d37b071aee2c1ffe18cfd97fdaa7d8a71
Rini Banerjee <rb2018@cam.ac.uk>
2025-08-06 16:13:43 +0100

    add a test

---
+++ tests/cn/contrived-goto.c
@@ -0,0 +1,46 @@
+/* A contrived example with labels and gotos, for making sure Fulminate's C control-flow machinery works */
+int f(int flag) {
+    int a = 'h';
+    if (flag) {
+      goto L1;
+    }
+    int b = 'w';
+  
+    {
+      int x = 1;
+  
+      {
+        int z = 42;
+        /*
+        ...
+        */
+        return 0;
+      }
+  
+    L1: {
+      int y = 2;
+      x = 5;
+      if (!flag) {
+        goto L2;
+      }
+      int a = 5;
+  
+      /*
+      ...
+      */
+    }
+  
+      /*
+      ...
+      */
+    }
+  
+  L2:
+    a = 's';
+    return 0;
+  }
+  
+  int main(void) {
+    f(1);
+    return 0;
+  }
