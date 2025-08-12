49616faf2cb68d21148927cc65999cf8f51fae21
Hiroyuki Katsura <moratorium08@gmail.com>
2025-08-12 14:50:41 +0100

    Store iterated resources in the context

--- runtime/libcn/src/cn-autoannot/focus_ctx.c
+++ runtime/libcn/src/cn-autoannot/focus_ctx.c
@@ -1,35 +1,36 @@
+#include <cn-autoannot/focus_ctx.h>
+
 #include <inttypes.h>
 #include <limits.h>
 #include <signal.h>  // for SIGABRT
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 #include <cn-executable/fulminate_alloc.h>
 #include <cn-executable/utils.h>
+#include <cn-autoannot/focus_ctx.h>
 
-// hashset
-typedef hash_table focus_set;
-
-struct focus_context {
-    focus_set* indices;
-    struct focus_context *prev;
-};
+// Note(HK):
+// we don't have to care about the difference between Owned/Block here
+// because they are handled by the standard Fulminate machinery.
+// i.e., if there is a discrepancy, it will be caught by Fulminate.
 
 struct focus_context* cn_focus_global_context;  // top of the stack
-
-void initialise_focus_context(void) {
-    cn_focus_global_context = fulm_default_alloc.malloc(sizeof(struct focus_context));
-    cn_focus_global_context->indices = ht_create(&fulm_default_alloc);
-    cn_focus_global_context->prev = NULL;
-}
 
 // should happen simultaneously with ghost_stack_depth_incr
 void push_focus_context(void) {
     struct focus_context* new_context = fulm_default_alloc.malloc(sizeof(struct focus_context));
-    new_context->indices = ht_create(&fulm_default_alloc);
+    new_context->indices = NULL;
+    new_context->iter_ress = NULL;
     new_context->prev = cn_focus_global_context;
     cn_focus_global_context = new_context;
+}
+
+void initialise_focus_context(void) {
+    push_focus_context();
 }
 
 void pop_focus_context(void) {
@@ -37,17 +38,64 @@ void pop_focus_context(void) {
     cn_focus_global_context = cn_focus_global_context->prev;
 }
 
-void insert_focus(int64_t index) {
-    int64_t* key = fulm_malloc(sizeof(int64_t), &fulm_default_alloc);
-    *key = index;
-    ht_set(cn_focus_global_context->indices, key, (void*)1);  // value is not used
+void insert_focus(int64_t index, type_sig sig) {
+    struct focus_set* new_set = fulm_malloc(sizeof(struct focus_set), &fulm_default_alloc);
+    new_set->info.index = index;
+    new_set->info.sig = sig;
+    new_set->next = cn_focus_global_context->indices;
+    cn_focus_global_context->indices = new_set;
 }
 
-int check_focus(int64_t index) {
-    if (ht_get(cn_focus_global_context->indices, &index) != NULL) {
+void insert_iter_res(uint64_t ptr, uint64_t size, uint64_t nelems, type_sig sig) {
+    struct iter_res_set* new = fulm_malloc(sizeof(iter_res_set), &fulm_default_alloc);
+    new->res.ptr = ptr;
+    new->res.size = size;
+    new->res.nelems = nelems;
+    new->res.sig = sig;
+    new->next = cn_focus_global_context->iter_ress;
+    cn_focus_global_context->iter_ress = new;
+}
+
+/// Checks if the given address
+///  (i) is in a iterated resource
+///  (ii) is not focused
+/// If (i) and (ii), it needs focus, and returns 1.
+int needs_focus(uint64_t address, uint64_t size, type_sig sig) {
+    assert(cn_focus_global_context != NULL);
+    // (i) search for iterated resource
+    iter_res_set *iter = cn_focus_global_context->iter_ress;
+    while (iter) {
+        iter_res_set *cur = iter;
+        iter = iter->next;
+        uint64_t start = cur->res.ptr;
+        uint64_t end = start + cur->res.size * cur->res.nelems;
+        uint64_t offset = address - start;
+        if (address < start || address + size > end) {
+            continue;
+        }
+        if (offset % cur->res.size != 0) {
+            continue;
+        }
+
+        uint64_t index = offset / cur->res.size;
+
+        if (strcmp(cur->res.sig, sig) != 0) {
+            continue;
+        }
+
+        // Case: an appropriate iterated resource is found
+        // (ii) search for focus
+        focus_set* cur_focus = cn_focus_global_context->indices;
+        while (cur_focus) {
+            if (cur_focus->info.index == index &&
+                strcmp(cur_focus->info.sig, sig) == 0) {
+                return 0;
+            }
+            cur_focus = cur_focus->next;
+        }
+        // The index is not focused
         return 1;
-    } else {
-        return 0;
     }
+    // We didn't find any appropriate iterated resource
+    return 0;
 }
-
