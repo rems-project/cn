e1be3bdeb08fc60c95fcdc5b63a7a7feb6b09852
Hiroyuki Katsura <moratorium08@gmail.com>
2025-08-12 14:50:41 +0100

    Store iterated resources in the context

--- runtime/libcn/include/cn-autoannot/focus_ctx.h
+++ runtime/libcn/include/cn-autoannot/focus_ctx.h
@@ -7,11 +7,43 @@
 extern "C" {
 #endif
 
+typedef const char *type_sig;
+
+// Limitation: we only consider contiguous iter ress.
+struct iter_res {
+  uint64_t ptr;
+  uint64_t size;
+  uint64_t nelems;
+  type_sig sig;
+};
+
+typedef struct iter_res_set {
+  struct iter_res res;
+  struct iter_res_set *next;
+} iter_res_set;
+
+struct focus_info {
+  int64_t index;
+  type_sig sig;
+};
+
+typedef struct focus_set {
+  struct focus_info info;
+  struct focus_set *next;
+} focus_set;
+
+struct focus_context {
+  focus_set *indices;
+  iter_res_set *iter_ress;
+  struct focus_context *prev;
+};
+
 void initialise_focus_context(void);
 void push_focus_context(void);
 void pop_focus_context(void);
-void insert_focus(int64_t index);
-int check_focus(int64_t index);
+void insert_focus(int64_t index, const char *type_sig);
+void insert_iter_res(uint64_t ptr, uint64_t size, uint64_t nelems, type_sig sig);
+int needs_focus(uint64_t address, uint64_t size, type_sig sig);
 
 #ifdef __cplusplus
 }
