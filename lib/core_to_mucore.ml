05026a24f31570924ec464c3c0cfe7a627089b49
Christopher Pulte <cp526@cam.ac.uk>
2025-09-23 21:34:29 +0100

    remove bool/int conversion special cases

--- lib/core_to_mucore.ml
+++ lib/core_to_mucore.ml
@@ -479,32 +479,10 @@ let rec n_pexpr ~inherit_loc loc (Pexpr (annots, bty, pe)) : unit Mucore.pexpr =
        let e'' = n_pexpr loc e'' in
        annotate (PElet (pat, e', e'')))
   | PEif (e1, e2, e3) ->
-    (match (e2, e3) with
-     | ( Pexpr (_, _, PEval (Vloaded (LVspecified (OVinteger iv1)))),
-         Pexpr (_, _, PEval (Vloaded (LVspecified (OVinteger iv2)))) )
-       when Option.equal Z.equal (CF.Mem.eval_integer_value iv1) (Some Z.one)
-            && Option.equal Z.equal (CF.Mem.eval_integer_value iv2) (Some Z.zero) ->
-       let e1 = n_pexpr loc e1 in
-       annotate (PEbool_to_integer e1)
-     | ( Pexpr
-           (_, _, PEctor (Cspecified, [ Pexpr (_, _, PEval (Vobject (OVinteger iv1))) ])),
-         Pexpr
-           (_, _, PEctor (Cspecified, [ Pexpr (_, _, PEval (Vobject (OVinteger iv2))) ]))
-       )
-       when Option.equal Z.equal (CF.Mem.eval_integer_value iv1) (Some Z.one)
-            && Option.equal Z.equal (CF.Mem.eval_integer_value iv2) (Some Z.zero) ->
-       let e1 = n_pexpr loc e1 in
-       annotate (PEbool_to_integer e1)
-     (* this should go away *)
-     | Pexpr (_, _, PEval Vtrue), Pexpr (_, _, PEval Vfalse) -> n_pexpr loc e1
-     | _ ->
-       let e1 = n_pexpr loc e1 in
-       let e2 = n_pexpr loc e2 in
-       let e3 = n_pexpr loc e3 in
-       (match e1 with
-        | Pexpr (_, _, _, PEval (V (_, Vtrue))) -> e2
-        | Pexpr (_, _, _, PEval (V (_, Vfalse))) -> e3
-        | _ -> annotate (PEif (e1, e2, e3))))
+     let e1 = n_pexpr loc e1 in
+     let e2 = n_pexpr loc e2 in
+     let e3 = n_pexpr loc e3 in
+     annotate (PEif (e1, e2, e3))
   | PEis_scalar _e' -> assert_error loc !^"core_anormalisation: PEis_scalar"
   | PEis_integer _e' -> assert_error loc !^"core_anormalisation: PEis_integer"
   | PEis_signed _e' -> assert_error loc !^"core_anormalisation: PEis_signed"
@@ -781,22 +759,10 @@ let rec n_expr
        let@ e2 = n_expr e2 in
        return (wrap (Elet (pat, e1, e2))))
   | Eif (e1, e2, e3) ->
-    (match (e2, e3) with
-     | ( Expr (_, Epure (Pexpr (_, _, PEval (Vloaded (LVspecified (OVinteger iv1)))))),
-         Expr (_, Epure (Pexpr (_, _, PEval (Vloaded (LVspecified (OVinteger iv2)))))) )
-       when Option.equal Z.equal (CF.Mem.eval_integer_value iv1) (Some Z.one)
-            && Option.equal Z.equal (CF.Mem.eval_integer_value iv2) (Some Z.zero) ->
-       let e1 = n_pexpr e1 in
-       return (wrap_pure (PEbool_to_integer e1))
-     | ( Expr (_, Epure (Pexpr (_, _, PEval Vtrue))),
-         Expr (_, Epure (Pexpr (_, _, PEval Vfalse))) ) ->
-       let e1 = n_pexpr e1 in
-       return (wrap (Epure e1))
-     | _ ->
-       let e1 = n_pexpr e1 in
-       let@ e2 = n_expr e2 in
-       let@ e3 = n_expr e3 in
-       return (wrap (Eif (e1, e2, e3))))
+     let e1 = n_pexpr e1 in
+     let@ e2 = n_expr e2 in
+     let@ e3 = n_expr e3 in
+     return (wrap (Eif (e1, e2, e3)))
   | Eccall (_a, ct1, e2, es) ->
     let ct1 =
       match ct1 with
