a652b253d5e938546e1b45dca14e7d158b1a5212
Rini Banerjee <rbanerjee20@gmail.com>
2025-11-04 17:33:19 +0000

    add opaque Fulm types to make Bennet runtime happy

---
+++ runtime/libcn/include/bennet/internals/fulm_types.h
@@ -0,0 +1,10 @@
+
+typedef struct cn_bits_u8 cn_bits_u8;
+typedef struct cn_bits_i8 cn_bits_i8;
+typedef struct cn_bits_u16 cn_bits_u16;
+typedef struct cn_bits_i16 cn_bits_i16;
+typedef struct cn_bits_u32 cn_bits_u32;
+typedef struct cn_bits_i32 cn_bits_i32;
+typedef struct cn_bits_u64 cn_bits_u64;
+typedef struct cn_bits_i64 cn_bits_i64;
+typedef struct cn_pointer cn_pointer;
