007aa79f872dd7617d31fa81df08705a9bab5406
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-11-05 13:58:41 -0500

    [Testing] Add an allocator wrapper

---
+++ runtime/libcn/include/cn-testing/memory/test_alloc.h
@@ -0,0 +1,56 @@
+#ifndef CN_TESTING_MEMORY_TEST_ALLOC_H
+#define CN_TESTING_MEMORY_TEST_ALLOC_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Allocate memory using the configured test allocator.
+ *
+ * @param size The number of bytes to allocate.
+ * @return A pointer to the allocated memory, or NULL if allocation fails.
+ */
+void* cn_test_malloc(size_t size);
+
+/**
+ * Allocate and zero-initialize memory using the configured test allocator.
+ *
+ * @param count The number of elements to allocate.
+ * @param size The size of each element in bytes.
+ * @return A pointer to the allocated and zeroed memory, or NULL if allocation fails.
+ */
+void* cn_test_calloc(size_t count, size_t size);
+
+/**
+ * Reallocate memory using the configured test allocator.
+ *
+ * @param ptr The pointer to the memory to reallocate, or NULL.
+ * @param size The new size in bytes.
+ * @return A pointer to the reallocated memory, or NULL if allocation fails.
+ */
+void* cn_test_realloc(void* ptr, size_t size);
+
+/**
+ * Allocate aligned memory using the configured test allocator.
+ *
+ * @param alignment The alignment requirement (must be a power of 2).
+ * @param size The number of bytes to allocate.
+ * @return A pointer to the aligned allocated memory, or NULL if allocation fails.
+ */
+void* cn_test_aligned_alloc(size_t alignment, size_t size);
+
+/**
+ * Free memory using the configured test allocator.
+ *
+ * @param ptr The pointer to the memory to free, or NULL.
+ */
+void cn_test_free(void* ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CN_TESTING_MEMORY_TEST_ALLOC_H */
---
+++ runtime/libcn/src/cn-testing/memory/test_alloc.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <cn-testing/memory/test_alloc.h>
+
+/**
+ * Internal allocator structure holding function pointers and context data.
+ */
+static struct {
+  void* data;
+  void* (*malloc)(void* data, size_t size);
+  void* (*calloc)(void* data, size_t count, size_t size);
+  void* (*realloc)(void* data, void* ptr, size_t size);
+  void* (*aligned_alloc)(void* data, size_t alignment, size_t size);
+  void (*free)(void* data, void* ptr);
+} cn_test_allocator = {
+    .data = NULL,
+    .malloc = NULL,
+    .calloc = NULL,
+    .realloc = NULL,
+    .aligned_alloc = NULL,
+    .free = NULL,
+};
+
+void* cn_test_malloc(size_t size) {
+  assert(cn_test_allocator.malloc != NULL);
+  return cn_test_allocator.malloc(cn_test_allocator.data, size);
+}
+
+void* cn_test_calloc(size_t count, size_t size) {
+  assert(cn_test_allocator.calloc != NULL);
+  return cn_test_allocator.calloc(cn_test_allocator.data, count, size);
+}
+
+void* cn_test_realloc(void* ptr, size_t size) {
+  assert(cn_test_allocator.realloc != NULL);
+  return cn_test_allocator.realloc(cn_test_allocator.data, ptr, size);
+}
+
+void* cn_test_aligned_alloc(size_t alignment, size_t size) {
+  assert(cn_test_allocator.aligned_alloc != NULL);
+  return cn_test_allocator.aligned_alloc(cn_test_allocator.data, alignment, size);
+}
+
+void cn_test_free(void* ptr) {
+  assert(cn_test_allocator.free != NULL);
+  cn_test_allocator.free(cn_test_allocator.data, ptr);
+}
