04ca9bb57959af6c1193619c55c94e20973bcb42
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-11-05 15:09:09 -0500

    [Testing] Wrap standard allocator

--- runtime/libcn/src/cn-testing/memory/test_alloc.c
+++ runtime/libcn/src/cn-testing/memory/test_alloc.c
@@ -14,6 +14,7 @@ static struct {
   void* (*realloc)(void* data, void* ptr, size_t size);
   void* (*aligned_alloc)(void* data, size_t alignment, size_t size);
   void (*free)(void* data, void* ptr);
+  void (*free_all)(void* data);
 } cn_test_allocator = {
     .data = NULL,
     .malloc = NULL,
@@ -21,6 +22,7 @@ static struct {
     .realloc = NULL,
     .aligned_alloc = NULL,
     .free = NULL,
+    .free_all = NULL,
 };
 
 void* cn_test_malloc(size_t size) {
@@ -48,16 +50,23 @@ void cn_test_free(void* ptr) {
   cn_test_allocator.free(cn_test_allocator.data, ptr);
 }
 
+void cn_test_free_all(void) {
+  assert(cn_test_allocator.free_all != NULL);
+  cn_test_allocator.free_all(cn_test_allocator.data);
+}
+
 void cn_test_set_alloc(void* data,
     void* (*malloc_fn)(void* data, size_t size),
     void* (*calloc_fn)(void* data, size_t count, size_t size),
     void* (*realloc_fn)(void* data, void* ptr, size_t size),
     void* (*aligned_alloc_fn)(void* data, size_t alignment, size_t size),
-    void (*free_fn)(void* data, void* ptr)) {
+    void (*free_fn)(void* data, void* ptr),
+    void (*free_all_fn)(void* data)) {
   cn_test_allocator.data = data;
   cn_test_allocator.malloc = malloc_fn;
   cn_test_allocator.calloc = calloc_fn;
   cn_test_allocator.realloc = realloc_fn;
   cn_test_allocator.aligned_alloc = aligned_alloc_fn;
   cn_test_allocator.free = free_fn;
+  cn_test_allocator.free_all = free_all_fn;
 }
