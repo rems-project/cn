1129f488df925396d7023dddb7cdff1b89de2858
Rini Banerjee <rbanerjee20@gmail.com>
2026-03-24 00:53:58 +0000

    [Fulminate] Optimise if-statements generated in conj loops a little

--- lib/indexTerms.ml
+++ lib/indexTerms.ml
@@ -1220,28 +1220,57 @@ module Bounds = struct
 
 
   (* used for Fulminate optimisation *)
-  let it_only_bounds ((x, _) : Sym.t * BT.t) (it : t) : bool =
+  let is_eq_bound ((x_sym, _) : Sym.t * BT.t) (it : t) : bool =
     match it with
-    | IT (Binop (EQ, IT (Sym x', _, _), _), _, _)
-    | IT (Binop (EQ, _, IT (Sym x', _, _)), _, _) ->
-      Sym.equal x x'
-    | IT (Binop (And, tm1, tm2), _, _) ->
-      let is_lower_bound it =
-        match it with
-        | IT (Binop (LE, _, IT (Sym x', _, _)), _, _)
-        | IT (Binop (LT, _, IT (Sym x', _, _)), _, _) ->
-          Sym.equal x x'
-        | _ -> false
-      in
-      let is_upper_bound it =
-        match it with
-        | IT (Binop (LE, IT (Sym x', _, _), _), _, _)
-        | IT (Binop (LT, IT (Sym x', _, _), _), _, _) ->
-          Sym.equal x x'
-        | _ -> false
-      in
-      is_lower_bound tm1 && is_upper_bound tm2
+    | IT (Binop (EQ, IT (Sym x_sym', _, _), _), _, _)
+    | IT (Binop (EQ, _, IT (Sym x_sym', _, _)), _, _) ->
+      Sym.equal x_sym x_sym'
     | _ -> false
+
+
+  let is_lower_bound ((x, _) : Sym.t * BT.t) (it : t) : bool =
+    match it with
+    | IT (Binop (LE, _, IT (Sym x', _, _)), _, _)
+    | IT (Binop (LT, _, IT (Sym x', _, _)), _, _) ->
+      Sym.equal x x'
+    | _ -> false
+
+
+  let is_upper_bound ((x, _) : Sym.t * BT.t) (it : t) : bool =
+    match it with
+    | IT (Binop (LE, IT (Sym x', _, _), _), _, _)
+    | IT (Binop (LT, IT (Sym x', _, _), _), _, _) ->
+      Sym.equal x x'
+    | _ -> false
+
+
+  let is_bound (x : Sym.t * BT.t) (it : t) : bool =
+    is_eq_bound x it || is_lower_bound x it || is_upper_bound x it
+
+
+  let it_only_bounds (x : Sym.t * BT.t) (it : t) : bool =
+    match it with
+    | IT (Binop (EQ, IT (Sym _, _, _), _), _, _) -> is_eq_bound x it
+    | IT (Binop (And, tm1, tm2), _, _) -> is_lower_bound x tm1 && is_upper_bound x tm2
+    | _ -> false
+
+
+  let it_non_bounds (x : Sym.t * BT.t) (it : t) : t =
+    let true_const = bool_ true Cerb_location.unknown in
+    let rec aux it =
+      match it with
+      | IT (Binop (And, tm1, tm2), bt, loc) ->
+        if is_bound x tm1 && is_bound x tm2 then
+          true_const
+        else if is_bound x tm1 then
+          aux tm2
+        else if is_bound x tm2 then
+          aux tm1
+        else
+          IT (Binop (And, aux tm1, aux tm2), bt, loc)
+      | _ -> it
+    in
+    aux it
 
 
   let get_bounds_opt ((x, bt) : Sym.t * BT.t) (it : t) : t option * t option =
