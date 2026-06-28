d577246d0fe0c2ab417f5ddbc95d2f20b8f2ed6c
Zain K Aamer <ZippeyKeys12@gmail.com>
2026-06-28 18:06:43 -0400

    [Bennet] Clean up pointer generation

--- runtime/libcn/include/bennet/state/alloc.h
+++ runtime/libcn/include/bennet/state/alloc.h
@@ -17,7 +17,8 @@ size_t bennet_alloc_save(void);
 void bennet_alloc_restore(size_t size);
 int bennet_alloc_check(void* p, size_t sz);
 void bennet_alloc_record(void* p, size_t sz);
-cn_pointer* bennet_alloc(bennet_domain(uintptr_t) * cs);
+void* bennet_alloc(size_t bytes);
+void* bennet_alloc_bounded(size_t bytes, uintptr_t lower_bound, uintptr_t upper_bound);
 
 void bennet_ownership_destroy(void);
 void bennet_ownership_init(void);
