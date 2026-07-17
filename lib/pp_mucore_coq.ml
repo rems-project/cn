6fe5ae4d93b620d3e3e6d09275d7c99c93859645
Christopher Pulte <cp526@cam.ac.uk>
2026-07-17 12:18:08 +0100

    more

--- lib/pp_mucore_coq.ml
+++ lib/pp_mucore_coq.ml
@@ -1981,7 +1981,7 @@ let coq_prologue =
       "From Cerberus Require Import Annot Core Ctype IntegerType Location Memory Symbol \
        Undefined Utils.";
       "From Cn Require Import ArgumentTypes BaseTypes CN CNProgs CNMem ErrorCommon False \
-       Id MakeTerm Locations LogicalArgumentTypes LogicalConstraints LogicalReturnTypes \
+       Id IndexTerms Locations LogicalArgumentTypes LogicalConstraints LogicalReturnTypes \
        MuCore Prooflog Request ReturnTypes Resource SCtypes Sym Terms."
     ]
 
