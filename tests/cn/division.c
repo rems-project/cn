fe56f291afb6a3ebafc0b7e1a98bbd1acfbd827b
Christopher Pulte <cp526@cam.ac.uk>
2026-09-01 12:10:43 +0100

    run CI tests with `--always-interp` and adjust test outputs accordingly

--- tests/cn/division.c
+++ tests/cn/division.c
@@ -2,8 +2,11 @@
    Must specify that the second operand is not equal to zero */
 
 int division (int x, int y)
-/*@ requires y != 0i32;
-    ensures return == x/y; @*/
+/*@ requires y != 0;
+             (y == (-1)) implies (x > MINi32());
+             let q = abs(x)/y;
+    ensures let r = (x < 0) ? (-q) : q;
+            return == r; @*/
 {
     return x / y;
 }
